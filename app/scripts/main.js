f0cb410a7b3ecbef9ca0e7c25d2913135940182a
Zainbawahab <Zainbawahab@zains-mbp.lan>
2015-05-02 01:23:15 -0400

    Added jQuery plugins and made siteresponsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,50 @@
   $(document).ready(function() {
     // Add your jQuery code here
 
+    var sticky = new Waypoint.Sticky({
+    	element: $('#nav-site2')[0]
+    });
+
+
+  	/*function makeSticky(){
+  		var myWindow = $(window),
+  			myHeader = $('#nav-site2');
+
+  		myWindow.scroll(function(){
+  			if(myWindow.scrollTop() === 0) {
+  			  myHeader.removeClass('sticky-nav');
+  			} else {
+  				myHeader.addClass('sticky-nav');
+  			}
+  		});
+  	}
+
+  	$(function(){
+  		makeSticky();
+  		$('#nav-site2').Waypoint('sticky');
+  	});*/
+
+  $('.is-zoomable').fluidbox({
+
+  });
+
+	/*"This is what I tried doing"
+	$('.is-zoomable').fluidbox(function(){
+  		$('Image').css('overflow', 'inherit');
+  	});*/
+
+  $('.Image').on('click',function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+  });
+
+  $('#nav-toggle').on('click', function(e) {
+    e.preventDefault();
+    $(this).add('#slidebar').toggleClass('is-open'); 
+  });
+
+
+
 
   });
 })(jQuery);
