b4d6688e95862abd0fbe48116a89c5a504c3383a
Zainbawahab <Zainbawahab@Zains-MacBook-Pro.local>
2015-05-04 23:40:12 -0400

    Gruntfile fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
     var config = {
         app: 'app',
         dist: 'dist'
+        components: 'bower_components'
     };
 
     // Define the configuration for all the tasks
@@ -210,6 +211,18 @@ module.exports = function (grunt) {
                   cwd: '<%= config.app %>/scripts',
                   dest: '<%= config.dist %>/scripts/',
                   src: '{,*/}*.js'
+                  },{
+                  expand: true,
+                  dot: true,
+                  cwd: '<%= config.components %>/Font-Awesome/fonts',
+                  dest: '<%= config.dist %>/fonts/',
+                  src: '*'
+                },{
+                  expand: true,
+                  dot: true,
+                  cwd: '<%= config.components %>/lightbox2/img',
+                  dest: '<%= config.dist %>/images/',
+                  src: '*.{png,gif}'
                 }]
             },
             styles: {
